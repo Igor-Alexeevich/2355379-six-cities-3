72415c2f82e83e2ebca988b0de12f1fcb479b612
Игорь Алексеевич <webpr@list.ru>
2026-06-02 00:13:46 +1000

    Сделаны 4 пункта задания

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,4 +1,10 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import FavoritesPage from '../../pages/favorites/favorites-page';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
+import { AppRoute } from '../../const';
 
 type AppScreenProps = {
   offersCount: number;
@@ -6,7 +12,30 @@ type AppScreenProps = {
 
 function App({ offersCount }: AppScreenProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path='*'
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,16 @@
+import PageNotFound from './pages/page-not-found/page-not-found';
+
 const setting = {
   offersCount: 312,
 } as const;
+
+// для переходов в меню
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+  PageNot = '/page-not-found'
+}
 
 export default setting;
